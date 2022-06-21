3a91e7b4b84709a27f70c3f8a5b9e1b7515f4885
matthias-aravena <94912264+matthias-aravena@users.noreply.github.com>
2022-06-21 16:08:26 -0300

    banner01

--- ProyectoArgentinaFinal/src/app/components/banner/banner.component.ts
+++ ProyectoArgentinaFinal/src/app/components/banner/banner.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Banner } from 'src/app/Model/banner.model';
+import { bannerService } from 'src/app/service/banner.service';
 
 @Component({
   selector: 'app-banner',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./banner.component.css']
 })
 export class BannerComponent implements OnInit {
-
-  constructor() { }
+  lista:any=[];
+  id: string ="";
+  editBanner:  Banner={id:'',imagenBanner: ''};
+  constructor(private BannerService: bannerService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit(): void {
+    this.id= this.activatedRoute.snapshot.params['id'];
+    this.BannerService.getunBanner(this.id).subscribe(
+      data=>{this.editBanner=data;}
+    );
+    this.listarBanner();
   }
+  listarBanner(){
+    this.BannerService.getBanners().subscribe(
+      res=>{this.lista=res}
+    );
+  }
+  modificarBanner(){
+    this.BannerService.editarBanner(this.id, this.editBanner).subscribe(
+      data=>{
+      this.router.navigate(['/home']);}
+     );
+    }
 
 }
---
+++ ProyectoArgentinaFinal/src/app/service/banner.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { Banner } from "../Model/banner.model";
+
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class bannerService {
+    url='http://localhost:8080/banner/banners';
+    constructor(private http: HttpClient) { }
+
+    getBanners():Observable<any>{
+        return this.http.get(this.url)
+    }
+
+    getunBanner(id: string):Observable<any>{
+        return this.http.get<any>(this.url+'/'+id);
+    }
+    guardarBanner(banner: Banner):Observable<any>{
+        return this.http.post(this.url, banner);
+    }
+    editarBanner(id: string, banner: Banner):Observable<any>{
+        return this.http.put<any>(this.url+'/'+id, banner); 
+    }
+
+    deleteBanner(id:string):Observable<any>{
+        return this.http.delete(this.url+'/'+id);
+    }
+}
